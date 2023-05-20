177b1176c14eb34cef04485a3422ee902206fdc2
Renan <felliphemouraa@gmail.com>
2023-05-19 21:50:26 -0300

    alteração no centro do menu aside

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-let list = document.querySelectorAll('.list');
+let list = document.querySelectorAll('.M-aside-list');
 
 function activeLink() {
     list.forEach((item) =>
@@ -9,7 +9,7 @@ list.forEach((item) =>
 
     item.addEventListener('click', activeLink));
 const body = document.querySelector("body"),
-    sidebar = body.querySelector(".menu-aside"),
+    sidebar = body.querySelector("M-menu-aside"),
     menu = body.querySelector(".menu-buttom");
 menu.addEventListener("click", () => {
     sidebar.classList.toggle("close");
