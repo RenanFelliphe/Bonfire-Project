4a4d44b6e8e31bb3cca4ef9699bdcdde60e7cb7d
Renan Felliphe <felliphemouraa@gmail.com>
2023-06-01 13:45:55 -0300

    Finalização do M-aside-center

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
+
+/*====================================================================================================
+============================================ MENU ASIDE ==============================================
+====================================================================================================*/
+
+//Estado de abertura e fechamento do Menu
 const list = document.querySelectorAll('.M-aside-list');
 const sidebar = document.querySelector(".M-menu-aside");
 const menuButton = document.querySelector(".menu-buttom");
@@ -6,7 +12,30 @@ menuButton.addEventListener('click', () => {
   sidebar.classList.toggle("close");
 });
 
+//Identação da página selecionada
 list.forEach(item => item.addEventListener('click', function() {
   list.forEach(item => item.classList.remove('active'));
   this.classList.add('active');
 }));
+
+//Troca de cor das barras
+const asideTop = document.querySelector('.M-aside-top');
+const firstSectionBefore = asideTop.querySelector('section:first-child::before');
+const asideCenter = document.querySelector('.M-aside-center');
+const menuAside = document.querySelector('.M-menu-aside');
+
+asideTop.addEventListener('mouseover', () => {
+  firstSectionBefore.classList.add('section-highlight');
+});
+
+asideTop.addEventListener('mouseout', () => {
+  firstSectionBefore.classList.remove('section-highlight');
+});
+
+asideCenter.addEventListener('mouseover', () => {
+  menuAside.classList.add('section-highlight');
+});
+
+asideCenter.addEventListener('mouseout', () => {
+  menuAside.classList.remove('section-highlight');
+});
