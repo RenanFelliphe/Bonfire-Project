dc5061bfa8a66bb366109bcd4519e9cc269de9d4
Renan Felliphe <felliphemouraa@gmail.com>
2023-10-02 18:15:00 -0300

    Adicionando um limitador de caracteres para as postagens

--- js/index.js
+++ js/index.js
@@ -7,10 +7,46 @@
 menuAsideFunctions(); //Funções e individualidades do MENU DE NAVEGAÇÃO LATERAL
 openQuickMessages(); //Abre e fecha a ÁREA DE MENSAGENS RÁPIDAS
 featuredProfilesFunctions(); //Funções da seção DESTAQUES
+postingFunctions(); //Funções da seção de postagem
 
 /*========================================================================================================================================
 ========================================================= DEFININDO AS FUNÇÕES ===========================================================
 /*=======================================================================================================================================*/
+
+//LIMITADOR de CARACTERES dos inputs
+function charactersLimit() {
+    const maxCharacters = parseInt(document.querySelector(".H-max-characters").textContent);
+    const input = document.querySelector(".H-type-here");
+    const inputContent = input.value;
+    const numberOFCharacters = document.querySelector(".H-characters-number");
+    const characteres = document.querySelector(".H-characters");
+
+    numberOFCharacters.textContent = input.value.length;
+    numberOFCharacters.style.color = "lime";
+
+    //100% do limite máximo de caracteres atingido
+    if (inputContent.length > maxCharacters) {
+        input.value = inputContent.slice(0, maxCharacters);
+        numberOFCharacters.textContent = "MAX";
+        numberOFCharacters.style.color = "red";
+
+        //Animação de shake/tremer
+        input.classList.add("characterBlocked");
+        characteres.classList.add("characterBlocked");
+        setTimeout(() => {
+            input.classList.remove("characterBlocked");
+            characteres.classList.remove("characterBlocked");
+        }, 700);
+
+    //70% do limite máximo de caracteres atingido
+    } else if(inputContent.length >= (maxCharacters/1.5)){
+        numberOFCharacters.style.color = "darkorange";
+    
+    //50% do limite máximo de caracteres atingido
+    } else if(inputContent.length >= (maxCharacters/2)){
+        numberOFCharacters.style.color = "yellow";
+    }
+}
 
 //Funções e individualidades do MENU DE NAVEGAÇÃO LATERAL
 function menuAsideFunctions() {
@@ -237,4 +273,5 @@ function featuredProfilesFunctions() {
     swiperProfiles(); 
     toggleFollows();
 }
-  
+
+
