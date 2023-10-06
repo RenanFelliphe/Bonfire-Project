1e27e0437563097cca9ce71ddccb357caf6a13de
Renan Felliphe <felliphemouraa@gmail.com>
2023-10-06 08:43:29 -0300

    Iniciando a implementação do Trending Topics

---
+++ requires/menu.php
@@ -0,0 +1,133 @@
+<nav class="M-menu-aside">
+    <span class="M-aside-gradient"> </span>
+    <section class="M-aside-top">
+        <div class="menu-button">
+            <i class="bi bi-list All-icon M-open"></i>
+            <i class="bi bi-x All-icon M-close"></i>
+        </div>
+
+        <div class="M-user-teams">
+            <i class="bi bi-circle-fill M-team-1"></i>
+            <i class="bi bi-circle-fill M-team-2"></i>
+            <i class="bi bi-circle-fill M-team-3"></i>
+        </div>
+    </section>
+    <section class="M-aside-center">
+        <ul>
+            <li class="M-list M-home active">
+                <a href="index.html" class="M-list-content">
+                    <span class="bi bi-house-door All-icon"></span>
+                    <span class="M-title"> Home </span>
+                </a>
+            </li>
+            <li class="M-list M-events">
+                <a class="M-list-content">
+                    <span class="bi bi-calendar-date All-icon"></span>
+                    <span class="M-title"> Eventos </span>
+                    <div class="M-event-icon">
+                        <img src="assets/loud-logo.png" alt="Logo do Criador do Evento">
+                    </div>
+                </a>
+            </li>
+            <li class="M-list M-search">
+                <a class="M-list-content">
+                    <span class="bi bi-globe-americas All-icon"></span>
+                    <span class="M-title"> Explorar </span>
+                </a>
+            </li>
+            <li class="M-list M-friends">
+                <a class="M-list-content">
+                    <span class="bi bi-people All-icon"></span>
+                    <span class="M-title"> Amigos </span>
+                    <span class="All-icon M-message-notify"></span>
+                </a>
+            </li>
+            <li class="M-list M-news">
+                <a class="M-list-content">
+                    <span class="bi bi-newspaper All-icon"></span>
+                    <span class="M-title"> News </span>
+                </a>
+            </li>
+            <li class="M-list M-games">
+                <a class="M-list-content">
+                    <span class="bi bi-joystick All-icon"></span>
+                    <span class="M-title"> Games </span>
+                </a>
+            </li>
+            <li class="M-list M-shopping">
+                <a class="M-list-content">
+                    <span class="bi bi-cart2 All-icon"></span>
+                    <span class="M-title"> Loja </span>
+                </a>
+            </li>
+            <li class="M-list M-more-pages">
+                <a class="M-list-content">
+                    <span class="bi bi-three-dots All-icon"></span>
+                    <span class="M-title"> Mais </span>
+                    <span class='bx bx-chevron-right All-icon All-arrows'></span>
+                </a>
+                <section class="M-aside-more close">
+                    <ul>
+                        <li class="M-sublist">
+                            <a class="M-list-content">
+                                <span class="bi bi-key All-icon"></span>
+                                <span class="M-title"> Cofre </span>
+                            </a>
+                        </li>
+                        <li class="M-sublist">
+                            <a class="M-list-content">
+                                <span class="bi bi-pie-chart All-icon"></span>
+                                <span class="M-title"> Estatísticas </span>
+                            </a>
+                        </li>
+                        <li class="M-sublist">
+                            <a href="config.html" class="M-list-content">
+                                <span class="bi bi-gear-wide All-icon"></span>
+                                <span class="M-title"> Configurações </span>
+                            </a>
+                        </li>
+                    </ul>
+                </section>
+            </li>
+        </ul>
+    </section>
+    <section class="M-aside-footer">
+        <div class="M-footer-content">
+            <figure class="M-user-images">
+                <a href="perfil.html">
+                    <div class="profile-img">
+                        <img src="assets/perfil.png" alt="Foto de Perfil">
+                    </div>
+                    <span class="icon-hover">Pesquisar</span>
+                </a>
+                <span class="M-user-status"></span>
+                <div class="org-img">
+                    <img src="assets/loud-logo.png" alt="Logo do Time">
+                </div>
+            </figure>
+
+            <div class="M-user-informations">
+                <p class="M-nick">Saadhak</p>
+                <p class="M-name">Matias Delipetro</p>
+            </div>
+
+            <i class="bi bi-arrow-left-right All-icon M-log-buttom "></i>
+            <div class="M-log-actions close">
+                <ul>
+                    <li>
+                        <a href="">
+                            <span class="bi bi-people All-icon"></span>
+                            <span class="M-title"> Alternar usuário </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="login.html">
+                            <span class="bi bi-box-arrow-left All-icon"></span>
+                            <span class="M-title M-log-out"> Sair </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+</nav>
