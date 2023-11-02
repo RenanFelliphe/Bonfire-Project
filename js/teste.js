220b43a89adfd183b71a0409c7304742d4ded58f
Renan Felliphe <felliphemouraa@gmail.com>
2023-11-02 20:24:24 -0300

    Consertando o centro do menu

--- js/teste.js
+++ js/teste.js
@@ -5,6 +5,17 @@ function menuAsideFunctions(){
     menuButton.addEventListener("click", () => {
         menu.classList.toggle("close");
     })
+        const logOptionsDiv = document.querySelector('.M-log-actions');
+        const logButton = document.querySelector('.M-log-button');
+        
+            logButton.addEventListener('click', function () {
+                logOptionsDiv.classList.toggle("close");
+                logButton.classList.toggle("active");
+            });
+    
+    
+
+    userActions();
 }
 
 menuAsideFunctions();
