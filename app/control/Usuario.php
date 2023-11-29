e02b961ea073d034cc3a7746e6fac3708f408a05
LiaLess1808 <nathaliacamposlessa1@gmail.com>
2023-11-29 18:39:34 -0300

    update Usuario.php

--- app/control/Usuario.php
+++ app/control/Usuario.php
@@ -1,45 +1,256 @@
 <?php 
+
+    // Criando Classe
+    //=====================================================================================================================================================
+    //=====================================================================================================================================================
+    //=====================================================================================================================================================
     class Usuario
     {
-        private $nome; 
-        private $apelido; 
-        private $email;  
-        private $senha; 
-        private $biografia;
-        private $genero; 
-        private $recado;  
+
+        private $nome = "a"; 
+        private $apelido = "a"; 
+        private $email ="@xyz.com";  
+        private $senha = "a"; 
+        private $biografia = "a";
+        private $genero = "a"; 
+        private $recado = "Nothing yet";  
         private $verificado = false;
-        private $nacionalidade;
+        private $nacionalidade = "BR";
         private $quantSeguidores = 0;
         private $quantSeguindo = 0;
+        private $quantPosts = 0;
 
-        function postar()
+        function __construct($nome,$apelido,$genero)
         {
-            echo "";
+            $this->nome = $nome;
+            $this->apelido = $apelido;  
+            $this->genero = $genero;
         }
 
-        function seguir($quant)
+        //Metodos
+        //=====================================================================================================================================================
+        //=====================================================================================================================================================
+        //=====================================================================================================================================================
+        function postar($post)
         {
-            $quantSeguindo = $quantSeguindo + $quant;
+            echo " $post (POST) <br>";
         }
 
-        function pararSeguir($quant)
+        function bemVindo()
         {
-            $quantSeguindo = $quantSeguindo - $quant;
+            if(strtolower($this->genero) === "masculino")
+            {
+                $gen = "o";
+                echo "Bem vind(". $gen .") " . $this->nome ." seu apelido está definido como \"" . $this->apelido ."\".<br>";
+            }
+            else if(strtolower($this->genero) === "feminino")
+            {
+                $gen = "a";
+                echo "Bem vind(". $gen .") " . $this->nome ." seu apelido está definido como \"" . $this->apelido ."\".<br>";
+            }
+            else if(!strtolower($this->genero) === "feminino" && !strtolower($this->genero) === "masculino")
+            {
+                $gen = "o/a";
+                echo "Bem vind(". $gen .") " . $this->nome ." seu apelido está definido como \"" . $this->apelido ."\".<br>";
+            }
         }
 
         function editarRecado($recado)
         {
-            $this -> $recado = $recado;
+            $this->recado = $recado;
+        }
+
+        public function changeVerificado()
+        {
+            if(($this->verificado)===true)
+            {
+                $this->verificado = false;
+            }
+            else
+            {
+                $this->verificado = true;
+            }
+        }
+
+        public function mostrarDados()
+        {
+            echo "Seguindo: " . $quantSeguindo . "<br>Seguidores:" . $quantSeguidores . "<br>Posts: " . $quantPosts;
+        }
+
+
+        //GETS
+        //=====================================================================================================================================================
+        //=====================================================================================================================================================
+        //=====================================================================================================================================================
+        public function getNome() 
+        {
+            return $this->nome;
+        }
+
+        public function getApelido() 
+        {
+            return $this->apelido;
+        }
+
+        public function getEmail() 
+        {
+            return $this->email;
+        }
+
+        public function getSenha() 
+        {
+            return $this->senha;
+        }
+
+        public function getBiografia() 
+        {
+            return $this->biografia;
+        }
+
+        public function getGenero() 
+        {
+            return $this->genero;
+        }
+
+        public function getRecado() 
+        {
+            return $this->recado;
+        }
+
+        public function getVerificado() 
+        {
+            return $this->verificado;
+        }
+
+        public function getNacionalidade()
+        {
+            return $this->nacionalidade;
+        }
+
+        public function getQtSeguidores()
+        {
+            return $this->quantSeguidores;
+        }
+
+        public function getQtSeguindo()
+        {
+            return $this->quantSeguindo;
+        }
+
+        public function getQtPosts()
+        {
+            return $this->quantPosts;
+        }
+
+        //SETS
+        //=====================================================================================================================================================
+        //=====================================================================================================================================================
+        //=====================================================================================================================================================
+        public function setNome($nome)
+        {
+            $this->nome = $nome;
+        }
+
+        public function setApelido($apelido)
+        {
+            $this->apelido = $apelido;
+        }
+
+        public function setEmail($email)
+        {
+            $this->email = $email;
+        }
+
+        public function setSenha($senha)
+        {
+            $this->senha = $senha;
+        }
+
+        public function setBiografia($biografia)
+        {
+            $this->biografia = $biografia;
+        }
+
+        public function setGenero($genero)
+        {
+            $this->genero = $genero;
+        }
+
+        public function setRecado($recado)
+        {
+            $this->recado = $recado;
+        }
+
+        public function setVerificado($verificado)
+        {
+            $this->verificado = $verificado;
+        }
+
+        public function setNacionalidade($nacionalidade)
+        {
+            $this->nacionalidade = $nacionalidade;
+        }
+
+        public function setQtSeguidores($quantSeguidores)
+        {
+            if($quantSeguidores <= 0)
+            {
+                $this->quantSeguidores = 0;
+            }
+            else
+            {
+                $this->quantSeguidores = $quantSeguidores;
+            }
+        }
+
+        public function setQtSeguindo($quantSeguindo)
+        {
+            if($quantSeguindo <= 0)
+            {
+                $this->quantSeguindo = 0;
+            }
+            else
+            {
+                $this->quantSeguindo = $quantSeguindo;
+            }
+        }
+
+        public function setQtPosts($quantPosts)
+        {
+            if($quantPosts <= 0)
+            {
+                $this->quantPosts = 0;
+            }
+            else
+            {
+                $this->quantPosts = $quantPosts;
+            }
         }
     }
 
-    $Renan = new Usuario;
-    $Nath = new Usuario;
+    //Instanciando Objetos
+    //=====================================================================================================================================================
+    //=====================================================================================================================================================
+    //=====================================================================================================================================================
 
-    $Renan -> seguir(10);
-    $Nath -> seguir(3);
+    $Renan = new Usuario("Renan Felliphe", "super peee", "Masculino");
+    $Nath = new Usuario("Nathália Campos", "mirrorNL2", "Feminino");
 
-    $Nath -> pararSeguir(1);
+    //Metodos
+    //=====================================================================================================================================================
+    //=====================================================================================================================================================
+    //=====================================================================================================================================================
+    $Renan->postar("GO LOUD!");
+    $Nath->postar("Me sentindo motivada!");
+
+    $Nath->editarRecado("We're Bonfire!");
+
+    //Construtores
+    //=====================================================================================================================================================
+    //=====================================================================================================================================================
+    //=====================================================================================================================================================
+    $genericUser = new Usuario("Natália da Mata", "nattmc", "Feminino");
+
+    $genericUser->bemVindo();
 
 ?>
