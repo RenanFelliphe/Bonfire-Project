c26feb0669d599547caf103394a2f9b2a59de74d
LiaLess1808 <nathaliacamposlessa1@gmail.com>
2023-11-29 10:45:22 -0300

    add POO to Usuario

--- app/control/Usuario.php
+++ app/control/Usuario.php
@@ -8,8 +8,38 @@
         private $biografia;
         private $genero; 
         private $recado;  
-        private $verificado;
+        private $verificado = false;
         private $nacionalidade;
+        private $quantSeguidores = 0;
+        private $quantSeguindo = 0;
 
+        function postar()
+        {
+            echo "";
+        }
+
+        function seguir($quant)
+        {
+            $quantSeguindo = $quantSeguindo + $quant;
+        }
+
+        function pararSeguir($quant)
+        {
+            $quantSeguindo = $quantSeguindo - $quant;
+        }
+
+        function editarRecado($recado)
+        {
+            $this -> $recado = $recado;
+        }
     }
+
+    $Renan = new Usuario;
+    $Nath = new Usuario;
+
+    $Renan -> seguir(10);
+    $Nath -> seguir(3);
+
+    $Nath -> pararSeguir(1);
+
 ?>
